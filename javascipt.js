7b6fb19ea1c41fe72bf2e0e9a56d2430327f1f54
rs27-prime <devilgaming7851@gmail.com>
2026-06-10 17:31:12 +0530

    basic code

---
+++ javascipt.js
@@ -0,0 +1,15 @@
+let a = 34
+var b = 30
+const c = 45
+
+// we used mainly const and let not var 
+
+let d = 56
+let e = 342455.6
+f = "Rushikesh"
+g = true
+h = null
+let i;
+let id = Symbol("id")
+
+
