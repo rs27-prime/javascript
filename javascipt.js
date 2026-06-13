9bcefcfc52c99bc0153618f4f444741754f9287e
rs27-prime <devilgaming7851@gmail.com>
2026-06-13 12:39:50 +0530

    variable

--- javascipt.js
+++ javascipt.js
@@ -2,14 +2,19 @@ let a = 34
 var b = 30
 const c = 45
 
-// we used mainly const and let not var 
 
 let d = 56
-let e = 342455.6
+let e = BigInt(1234567890123456789012345678901234567890)
 f = "Rushikesh"
 g = true
 h = null
 let i;
 let id = Symbol("id")
+
+let dictionary = {
+    name: "Rushikesh",
+    age: 23,
+    city: "Pune"
+}
 
 
