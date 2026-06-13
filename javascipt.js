952295966e636e2c2ccbc871ed306abc2b8067ef
rs27-prime <devilgaming7851@gmail.com>
2026-06-13 12:50:51 +0530

    operator

---
+++ javascipt.js
@@ -0,0 +1,36 @@
+// Operators 
+let a = 56
+let b = 34
+
+console.log("a + b = " + (a + b)); // addition
+console.log("a - b = " + (a - b)); // subtraction
+console.log("a * b = " + (a * b)); // multiplication
+console.log("a / b = " + (a / b)); // division
+console.log("a % b = " + (a % b)); // modulus
+console.log("a ** b = " + (a ** b)); // exponentiation
+
+console.log("a += b: " + (a += b)); // addition assignment
+console.log("a -= b: " + (a -= b));
+
+
+console.log("a == b: " + (a == b)); // equal to
+console.log("a === b: " + (a === b)); // equal value and equal type
+console.log("a != b: " + (a != b)); // not equal to
+console.log("a > b: " + (a > b));   // greater than
+console.log("a < b: " + (a < b));   // less than
+console.log("a >= b: " + (a >= b)); // greater than or equal to
+console.log("a <= b: " + (a <= b)); // less than or equal to
+
+console.log("a && b: " + (a && b)); // logical AND
+console.log("a || b: " + (a || b)); // logical OR
+console.log("!a: " + (!a));    // logical NOT
+console.log("!b: " + (!b));    // logical NOT
+
+console.log("++a: " + (++a)); // increment
+console.log("--b: " + (--b)); // decrement
+console.log("a++: " + (a++)); // increment
+console.log("b--: " + (b--)); // decrement
+
+
+
+
