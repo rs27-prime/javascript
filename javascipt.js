fa41e57284b6cbf5f6af9d379b45898a2f06a225
rs27-prime <devilgaming7851@gmail.com>
2026-06-13 15:20:14 +0530

    expression

--- javascipt.js
+++ javascipt.js
@@ -1,13 +1,24 @@
+let a = prompt("enter your age");
+if (a < 0) {
+  alert("enter valid age");
+} else if (a > 0 && a < 18) {
+  alert("you can't drive");
+} else {
+  alert("you are eligible for driving test");
+}
 
-
-let a = 45 // let used for declaring a variable that can be reassigned
-const b = 67 // const used for declaring a variable that cannot be reassigned
-var c = 89 // var used for declaring a variable that can be reassigned and has function scope
-
-console.log(a) //used for printing the value of a variable
-prompt("Enter your name") //used for taking input from the user
-alert("Hello World") //used for showing an alert message to the user
-
-
-let age = prompt("Enter your age") //used for taking input from the user
-age = parseInt(age) //used for converting a string to an integer
+let b = prompt("enter your age");
+b = parseInt(b);
+switch (b) {
+  case 1:
+    alert("you can drive");
+    break;
+  case 2:
+    alert("you are not allowed");
+    break;
+  case 18:
+    alert("you are allowed");
+    break;
+  default:
+    alert("invalid");
+}
