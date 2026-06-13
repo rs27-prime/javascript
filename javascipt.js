56671b3ec97ced45d2e0e9ff7c70c48a8be07945
rs27-prime <devilgaming7851@gmail.com>
2026-06-13 13:00:26 +0530

    basic java script

---
+++ javascipt.js
@@ -0,0 +1,11 @@
+
+
+let a = 45 // let used for declaring a variable that can be reassigned
+const b = 67 // const used for declaring a variable that cannot be reassigned
+var c = 89 // var used for declaring a variable that can be reassigned and has function scope
+
+console.log(a) //used for printing the value of a variable
+prompt("Enter your name") //used for taking input from the user
+alert("Hello World") //used for showing an alert message to the user
+
+
