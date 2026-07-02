f7d4778f5055c5815c2c2f349b049207f887b2fd
rs27-prime <devilgaming7851@gmail.com>
2026-07-02 21:47:18 +0530

    table operation

--- javascipt.js
+++ javascipt.js
@@ -1,24 +1,7 @@
-console.log("show message in console");
-console.error("this show error");
-console.assert(5>6) // check condition only if true
-console.warn("this is the warning")
-console.info("this is important message");
- 
- let obj = {a:2,b:3,c:4,d:5}
- console.table(obj)
+document.body.firstElementChild.firstElementChild // go into depth
+document.body.firstElementChild.tHead
+document.body.firstElementChild.tbody
+document.body.firstElementChild.row
+document.body.firstElementChild.column
+document.body.firstElementChild.caption
 
- console.time("a") // while checking time runtime
- console.timeEnd("a")
-
-let a = prompt("enter the number : ", "27")
-alert("your script is working")
-
-document.write(a)
-let write = confirm("do you want me to write")
-if(write){
-document.write(a)    
-}
-else{
-    document.write("please allowed me to write");
-    
-}
