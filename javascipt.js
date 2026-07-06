ef17f873d770b8446844899044dc2fad47c89464
rs27-prime <devilgaming7851@gmail.com>
2026-07-06 20:44:47 +0530

    class add and deletation option

---
+++ javascipt.js
@@ -0,0 +1,12 @@
+// rushi.insertAdjacentHTML('beforeend', '<h1>Hello, World!</h1>');
+// rushi.insertAdjacentHTML('beforebegin', '<p>This is a simple JavaScript example.</p>');
+// rushi.insertAdjacentHTML('afterend', '<button id="myButton">Click Me!</button>');
+// rushi.insertAdjacentHTML('afterbegin', '<div id="myDiv">This is a div element.</div>');
+
+
+rushi.className = "red green black";
+rushi.classList
+rushi.classList.add("blue");
+rushi.classList.remove("green");
+rushi.classList.toggle("black");
+rushi.classList.contains("black");
