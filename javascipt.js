1e7e88cd00a1590b70e233dffc32dac2f8021e92
rs27-prime <devilgaming7851@gmail.com>
2026-07-06 18:44:51 +0530

    for checking

--- javascipt.js
+++ javascipt.js
@@ -1,7 +1,27 @@
-document.body.firstElementChild.firstElementChild // go into depth
-document.body.firstElementChild.tHead
-document.body.firstElementChild.tbody
-document.body.firstElementChild.row
-document.body.firstElementChild.column
-document.body.firstElementChild.caption
 
+// let usedID = document.getElementById("firstPara")
+// usedID.style.background = "green"
+
+// let usedClass = document.getElementsByClassName("byClass")[3]
+// usedClass.style.background = "yellow"
+
+// let usedCssSelector = document.querySelectorAll(".usedCSS")
+// usedCssSelector.style.color = "red"
+
+// let first = document.getElementById("first")
+// first.style.background = "green" 
+// console.log(first.matches(".box"));
+// console.log(first.closest(".box"));
+// console.log(first.contains(".check"));
+
+// console.dir(document.getElementsByTagName('span')[0]); full details 
+// node name give information about node
+
+
+// document.body.firstChild.nodeName  
+// document.body.firstChild.nodeValue   
+// document.body.firstChild.data   
+// document.body.firstChild.textContent
+// hidden for hide    
+// data.innerHTML   
+// data.outerHTML
