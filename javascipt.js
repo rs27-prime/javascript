56b75b4f070409bb55360d14afba1897d8895bb5
rs27-prime <devilgaming7851@gmail.com>
2026-07-07 15:38:43 +0530

    nothing

--- javascipt.js
+++ javascipt.js
@@ -1,14 +1,1 @@
-// event 
-
-// const add = (a,b)=>{
-//     return console.log((a+b));   
-// }
-// let a = setTimeout(add(4,5), 1000)
-// let b = setInterval(add(4,5), 1000)
-
-// clearTimeout(a)
-// clearInterval(b)
-
-
-// addEventListener
-// removeEventListener
+    
