70fb55fa2a5f4d44f473590cc4b52b2b24139f4f
rs27-prime <devilgaming7851@gmail.com>
2026-07-25 11:00:37 +0530

    try catch pending

--- javascipt.js
+++ javascipt.js
@@ -1,1 +1,23 @@
-hi 
+try {
+    throw new ReferenceError("Rushi is too good")
+} catch (error) {
+    console.log(error.message);
+    console.log(error.name);
+    console.log(error.stack);   
+}
+
+
+
+try {
+    console.log("if this not run");
+    console.log(program);
+    
+} catch (error) {
+    console.log("show this error");
+    
+}
+
+finally{
+    console.log("this run even after error");
+    
+}
